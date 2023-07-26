840776429d20f3d2cadb79ec2069c396bfc300a7
sorbon01 <sanginov.sorbon01@gmail.com>
2023-07-26 14:20:17 +0500

    add component clips-list

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -11,6 +11,8 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 })
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>
+  pageClips: IClip[] = []
+  pendingReq = false
 
   constructor(
     private db: AngularFirestore,
@@ -57,8 +59,39 @@ export class ClipService {
 
   async deleteClip(clip:IClip){
     const clipRef = this.storage.ref(`clips/${clip.fileName}`)
+    const screenshotRef = this.storage.ref(`screenshots/${clip.screenshotFileName}`)
 
+    await screenshotRef.delete()
     await clipRef.delete()
     await this.clipsCollection.doc(clip.docID).delete()
+  }
+
+  async getClips(){
+    if(this.pendingReq){
+      return
+    }
+    this.pendingReq = true
+    let query = this.clipsCollection.ref.orderBy('timestamp','desc').limit(6)
+
+    const { length } = this.pageClips
+
+    if(length){
+      const lastDocID = this.pageClips[length-1].docID
+      const lastDoc = await this.clipsCollection.doc(lastDocID)
+        .get()
+        .toPromise()
+      query = query.startAfter(lastDoc)
+    }
+
+    const snapshot = await query.get()
+
+    snapshot.forEach(doc => {
+      this.pageClips.push({
+        docID: doc.id,
+        ...doc.data()
+      })
+    })
+
+    this.pendingReq = false
   }
 }
