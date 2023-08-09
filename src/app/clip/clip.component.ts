313a67cd9d3b58ab32eac335856fd47c745511a5
sorbon01 <sanginov.sorbon01@gmail.com>
2023-08-09 16:42:09 +0500

    Completed Project

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import videojs from 'video.js';
 import Player from "video.js/dist/types/player";
+import IClip from '../models/clip.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.css']
+  styleUrls: ['./clip.component.css'],
+  encapsulation:ViewEncapsulation.None,
+  providers:[DatePipe]
 })
 export class ClipComponent implements OnInit {
-  id = ''
   @ViewChild('videoPlayer', {static:true}) target?:ElementRef
   player?:  Player;
+  clip?:IClip
 
   constructor(public route:ActivatedRoute, ) { }
 
   ngOnInit(): void {
     this.player = videojs(this.target?.nativeElement)
 
-    this.route.params.subscribe((params:Params) =>
-    this.id = params['id'])
+    this.route.data.subscribe(data => {
+      this.clip = data['clip'] as IClip
+
+      this.player?.src({
+        src:this.clip.url,
+        type:'video/mp4'
+      })
+    })
   }
 
 }
--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -3,13 +3,15 @@ import { AngularFirestore, AngularFirestoreCollection, DocumentReference, QueryS
 import IClip from '../models/clip.model';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { switchMap, map } from 'rxjs/operators';
-import { of,BehaviorSubject, combineLatest } from 'rxjs';
+import { of,BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot, Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClipService {
+export class ClipService implements Resolve<IClip | null>{
   public clipsCollection: AngularFirestoreCollection<IClip>
   pageClips: IClip[] = []
   pendingReq = false
@@ -17,7 +19,8 @@ export class ClipService {
   constructor(
     private db: AngularFirestore,
     private auth:AngularFireAuth,
-    private storage: AngularFireStorage
+    private storage: AngularFireStorage,
+    private router: Router
   ) { 
     this.clipsCollection = db.collection('clips')
   }
@@ -94,4 +97,23 @@ export class ClipService {
 
     this.pendingReq = false
   }
+
+  resolve(route:ActivatedRouteSnapshot, state:RouterStateSnapshot) {
+    return this.clipsCollection.doc(route.params['id'])
+                  .get()
+                  .pipe(
+                    map(snapshot =>{
+                      const data = snapshot.data()
+
+                      if(!data){
+                        this.router.navigate(['/'])
+                        return null
+                      }
+
+                      return data
+                    })
+                  )
+  }
+
+
 }
